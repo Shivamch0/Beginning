89885b4d0c0b6f9985b31838b6b0afd622b4774c
Student Shivam <115865754+Shivamch0@users.noreply.github.com>
2024-11-17 17:29:47 +0530

    GitHub Profile App

---
+++ Project 16/app.js
@@ -0,0 +1,75 @@
+const API = "https://api.github.com/users/";
+const main = document.querySelector("#main");
+const searchBox = document.querySelector("#search");
+
+const getUser = async (username) => {
+  const response = await fetch(API + username);
+  const data = await response.json();
+  const card = `
+   <div class="card">
+            <div>
+                <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
+            </div>     
+        <div class="user-info">
+            <h2>${data.name}</h2>
+            <p>${data.bio}</p>
+            <ul class="info">
+                <li>${data.followers}<strong>Followers</strong></li>
+                <li>${data.following}<strong>Following</strong></li>
+                <li>${data.public_repos}<strong>Repos</strong></li>
+            </ul>
+
+            <div class="repos">
+               
+            </div>
+        </div>
+        </div>   
+    `;
+  main.innerHTML = card;
+  getRepos(username);
+};
+
+// const search = document.querySelector("#search");
+
+// search.addEventListener(
+//     "keyup" ,
+//     function(){
+
+//     }
+// )
+
+// init call
+
+const getRepos = async (username) => {
+  const repoBox = document.querySelector(".repos");
+  const response = await fetch(API + username + "/repos");
+  const data = await response.json();
+  data.forEach(
+    (item) => {
+    const elem = document.createElement("a");
+    elem.classList.add("repo");
+    elem.href = item.html_url;
+    elem.innerText = item.name;
+    elem.target = "_blank";
+    repoBox.appendChild(elem);
+  });
+};
+
+const forSubmit = () => {
+    if(searchBox.value != ""){
+        getUser(searchBox.value);
+        searchBox.value = "";
+    }
+    return false;
+}
+
+searchBox.addEventListener(
+    "focusout",
+    function () {
+        forSubmit();
+    }
+)
+
+//  <a class="repo" href="#" target="_blank">Repo 1</a>
+// <a class="repo" href="#" target="_blank">Repo 2</a>
+// <a class="repo" href="#" target="_blank">Repo 3</a> //
